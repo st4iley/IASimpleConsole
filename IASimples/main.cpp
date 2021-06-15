54f18ae7a1d80541fbddd574c6c09a0f26d3ef0b
st4iley <81092236+st4iley@users.noreply.github.com>
2021-06-15 16:24:21 -0300

    Add files via upload

---
+++ IASimples/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+string name;
+string sair = "sair";
+
+int main(){
+	
+	cout<<"digite seu nome:";
+	cin>>name;
+	cout<<"ola ";
+	cout<<name << endl;
+	cout<<"oque vc deseja"<<endl;
+	cin>>sair;
+	cout<<"esta bem ";
+	cout<<name <<endl;
+	cout<<"saindo"<<endl;
+	system("pause");
+	return 0;
+	
+}
